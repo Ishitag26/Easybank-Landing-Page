76aab98cc61eaf2351ae801e4fa681aa342750a4
Ishita-gup123 <85993726+Ishita-gup123@users.noreply.github.com>
2021-07-22 07:16:46 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
 const navList = document.querySelector(".nav-list");
 const toggler = document.querySelector(".toggle-btn");
-const toggleImg = document.querySelector("toggle-img");
 
-toggler.addEventListener("click",()=>{
-    const previousImg = toggleImg.getAttribute("src");
-    navList.classList.toggle("mobile");
+const toggleImg = toggler.querySelector(".toggle-img");
 
-    if(navList.classList.contains("mobile")===false){
-        toggleImg.setAttribute("src","../images/icon-hamburger.svg");
-    }
-    else{
-        toggleImg.setAttribute("src","../images/icon-close.svg");
-    }
+toggler.addEventListener("click", () => {
+  const previousImg = toggleImg.getAttribute("src");
+  navList.classList.toggle("mobile");
+
+  if (navList.classList.contains("mobile") === false) {
+    toggleImg.setAttribute("src", "../images/icon-hamburger.svg");
+  } else {
+    toggleImg.setAttribute("src", "../images/icon-close.svg");
+  }
 });
 
-//media query list when window size matches the (min-width:1000px)
+// media query list when the window size matches the (min-width: 1000px)
 const mquery = window.matchMedia("(min-width: 1000px)");
 
-//remove the mobile class from  navlist on desktop mode(when the window is wider than 1000px) and set the toggle image back to the hamburger icon
-mquery.addListener(() =>{
-    if(mquery.matches){
-        navList.classList.remove("mobile");
-        toggleImg.setAttribute("src","../images/icon-hamburger.svg");
-    }
+// remove the mobile class from navlist on desktop mode(when the window is wider than 1000px) and set the toggler image back to the hamburger icon
+mquery.addListener(() => {
+  if (mquery.matches) {
+    navList.classList.remove("mobile");
+    toggleImg.setAttribute("src", "../images/icon-hamburger.svg");
+  }
 });
+
+// mq.addEventListener("change", () => {
+//   console.log("called");
+//   navList.classList.remove("mobile");
+// });
